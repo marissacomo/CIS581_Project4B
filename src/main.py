135a09c8651e0c8c1b8a59561ae978546d83d464
daedalus5 <zach.corse@gmail.com>
2018-12-14 16:23:46 -0500

    change

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import dlib as dl
 from scipy.ndimage import geometric_transform
 from scipy import ndimage
 
-def main():
+def main(video):
+  
+
   return
 
 
-main()
+main('../Easy/JonSnow.mp4')
